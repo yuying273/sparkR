f9d0b64972d53e78417201f3f2c902a9153c4784
Yuying Song <syysea@gmail.com>
2017-04-18 00:59:43 -0400

    Update logistic.py

--- logistic.py
+++ logistic.py
@@ -1,1 +1,15 @@
 
+from pyspark.mllib.classification import LogisticRegressionWithLBFGS
+from pyspark.mllib.regression import LabeledPoint
+from numpy import array
+data = sc.textFile("/wsc/song273/sk/test/logistics2.txt")
+data.count()
+data.first()
+def parsePoint(line):
+  values = [float(x) for x in line.split(' ')]
+  return LabeledPoint(values[127], values[0:127])
+
+parsedData = data.map(parsePoint)
+model = LogisticRegressionWithLBFGS.train(parsedData)
+## 
+
