14e4d80bedb7eea08a4ee3565e8ca6808332442d
Yuying Song <syysea@gmail.com>
2017-04-19 18:54:19 -0400

    Update logistic.py

--- logistic.py
+++ logistic.py
@@ -17,5 +17,53 @@ def parsePoint(line):
 
 parsedData = data.map(parsePoint)
 model = LogisticRegressionWithLBFGS.train(parsedData)
-## 
+## huayi's code
+
+# cd /var/lib/spark
+# ./bin/pyspark --master yarn-client --num-executors 5 --executor-memory 10g --executor-cores 5
+# ./bin/pyspark --master yarn-client --num-executors 5 --executor-cores 5
+from pyspark import SparkContext, SparkConf
+import random
+from pyspark.mllib.classification import LogisticRegressionWithLBFGS
+from pyspark.mllib.regression import LabeledPoint
+import csv
+from numpy import random as rd
+import numpy as np
+
+conf = SparkConf().setMaster("yarn-client").setAppName("logistic").set("spark.executor.memory","1g")
+sc = SparkContext(conf=conf)
+
+
+
+def generate(line):
+    p = 15
+    m = 2 ^ 8
+    np.random.seed(line)
+    x = np.random.normal(0, 1, [m, p])
+    np.random.seed(line)
+    y = np.random.choice(0, 1, [m, 1])
+    dataframe = np.hstack((x, y))
+    return dataframe
+
+
+
+
+
+#logFile = "$YOUR_SPARK_HOME/README.md"  # Should be some file on your system
+#sc = SparkContext("local", "Simple App")
+
+# with open('numbers.csv', 'wb') as fp:
+#  a = csv.writer(fp)
+#         data = np.zeros((1, 1))
+#         data[0, 0] = 1
+#  for i in range(100):
+#     a.writerows(data)
+#                 data[0, 0] += 1
+#
+# data = sc.textFile("C:/Users/Justin/Desktop/Spark_BoxCox/numbers.csv")
+
+data = sc.parallelize(range(0,100), 2)
+
+
+genDataFrame = data.map(generate)
 
