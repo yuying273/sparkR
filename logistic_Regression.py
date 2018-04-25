e7396d6af250064c73da4c45bcae620454ba71ae
Yuying Song <syysea@gmail.com>
2018-04-25 00:08:16 -0400

    Update logistic_Regression.py

--- logistic_Regression.py
+++ logistic_Regression.py
@@ -6,10 +6,41 @@
 #and then across the partitions, allowing it to scale efficiently to large datasets.
 
 ## carry out the logistic regression and return the average value for coefficient
+
+from pyspark import SparkContext, SparkConf
+from pyspark.ml.regression import GeneralizedLinearRegression
+from pyspark.mllib.regression import LabeledPoint
+import numpy as np
+import math
+
+n = 29
+v = 4
+p = 2**v-1
+m = 8
+r = 2**(n-m)
+
+def logisT(value):
+  glr = GeneralizedLinearRegression(family="gaussian", link="identity", maxIter=10, regParam=0.3)
+  # Fit the model
+  model = glr.fit(value)
+  # Print the coefficients and intercept for generalized linear regression model
+  print("Coefficients: " + str(model.coefficients))
+  print("Intercept: " + str(model.intercept))
+  return (model.coefficients,1)
+
+
+inputfile = "/wsc/song273/spark/data/n" + str(n)+"v"+str(v) + "m" + str(int(math.log(m,2)))
+rdd = sc.textFile(inputfile)
+
+coefs = rdd.mapValues(logisT).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
+
+
+
+
 ## reduceByKey, familiar with the combiner concept from MapReduce
 ## calling reduceByKey() and foldByKey() will automatically perform combining locally
 ## on each machine before computing global totals for each key. The user does not need to specify a combiner.
 The more general combineByKey() interface allows you to customize combining behavior.
-rdd.mapValues(lambda x: (x, 1)).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
+rdd.mapValues(logisT).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
 
 ##
