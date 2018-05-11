cbe19055fcc44295ec3e0764bf799122f8375de2
Yuying Song <syysea@gmail.com>
2018-05-11 17:01:27 -0400

    Update large_csv_write.py

--- large_csv_write.py
+++ large_csv_write.py
@@ -12,14 +12,16 @@ import uuid
 import csv
 import os
 outfile = '/hadoop/mnt/wsc/song273/try.csv'
-outsize = 10 # MB
-chunksize = 1000
+''m: subtset size; r:subset numbers''
+m = 
+r = 
 with open(outfile, 'a') as csvfile:
-        data = [
+        for i in range(r):
+                data = [
                 #[uuid.uuid4() for i in range(chunksize)],
-                np.random.random(chunksize)*50,
-                np.random.random(chunksize)*50,
-                np.random.randint(1000, size=(chunksize,))]
-        #csvfile.writelines(['%s,%.6f,%.6f,%i\n' % row for row in zip(*data)])   
-        csvfile.writelines(['%.6f,%.6f,%i\n' % row for row in zip(*data)])   
+                        np.random.random(chunksize)*50,
+                        np.random.random(chunksize)*50,
+                        np.random.randint(1000, size=(chunksize,))]
+                #csvfile.writelines(['%s,%.6f,%.6f,%i\n' % row for row in zip(*data)])   
+                csvfile.writelines(['%.6f,%.6f,%i\n' % row for row in zip(*data)])   
 
