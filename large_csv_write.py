9a80c16c89141892873ff7aa4819d27c81cfbad0
Yuying Song <syysea@gmail.com>
2018-05-11 17:14:46 -0400

    Update large_csv_write.py

--- large_csv_write.py
+++ large_csv_write.py
@@ -13,15 +13,29 @@ import csv
 import os
 outfile = '/hadoop/mnt/wsc/song273/try.csv'
 ''m: subtset size; r:subset numbers''
-m = 
-r = 
+m = 6
+r = 10
+p = 20
+def generate(line,m=m,p=p):
+    p = p
+    m = 2**m
+    np.random.seed(line)
+    x = np.random.normal(0, 1, [m, p])
+    np.random.seed(line)
+    #y = np.random.choice([0, 1], [m, 1])
+    y = np.random.binomial(2,0.5,[m,1])
+    subset_id = 
+    dataframe = np.hstack((x, y,line))
+    return np.ndarray.tolist(dataframe) 
+
 with open(outfile, 'a') as csvfile:
         for i in range(r):
-                data = [
+                #data = [
                 #[uuid.uuid4() for i in range(chunksize)],
-                        np.random.random(chunksize)*50,
-                        np.random.random(chunksize)*50,
-                        np.random.randint(1000, size=(chunksize,))]
-                #csvfile.writelines(['%s,%.6f,%.6f,%i\n' % row for row in zip(*data)])   
-                csvfile.writelines(['%.6f,%.6f,%i\n' % row for row in zip(*data)])   
+                #        np.random.random(chunksize)*50,
+                #        np.random.random(chunksize)*50,
+                #        np.random.randint(1000, size=(chunksize,))]
+                #csvfile.writelines(['%s,%.6f,%.6f,%i\n' % row for row in zip(*data)])
+                data = generate(i,m,p)
+                csvfile.writelines(['%.6f,'*p+'%i\n'*2 % row for row in zip(*data)])   
 
