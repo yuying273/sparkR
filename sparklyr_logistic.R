29d371b871686a1a03daa3b02f9654f70eefe7dd
Yuying Song <syysea@gmail.com>
2018-05-14 17:53:19 -0400

    Update sparklyr_logistic.R

--- sparklyr_logistic.R
+++ sparklyr_logistic.R
@@ -4,16 +4,37 @@ library(dplyr)
 
 ## timing
 sleep = 60
-
-## start time
+n=20
+v=4
+m=10
+datapath = paste("/wsc/song273/pf10n/sparklyr/parquet/","n",n,"v",v,"m",m,sep="")
+################################# start time ##################################
 start_time <- Sys.time()
-data_tbl = spark_read_parquet(sc,"groupdata2","/wsc/song273/pf10n/sparklyr/parquet/n20v4m10")
+
+config <- spark_config()   # Create a config to tune memory
+
+# no need to set spark_home, hadoop_conf_dir and so on, it is set on wsc cluster, you can check
+# Sys.getenv('SPARK_HOME')
+#Sys.setenv(SPARK_HOME = "/usr/lib/spark")
+#Sys.setenv(HADOOP_CONF_DIR = '/usr/lib/hadoop/etc/hadoop')
+#Sys.setenv(YARN_CONF_DIR = '/usr/lib/hadoop/etc/hadoop/conf')
+
+#config$spark.executor.instances <- 2
+## WSC cluster has 10 node, each node has 20 cores
+config$spark.executor.cores <- 20
+config$spark.dynamicAllocation.enabled = TRUE
+#config$spark.executor.memory <- "4G"
+#config[["sparklyr.shell.driver-memory"]] <- "10G"   # Set driver memory to 10GB
+# create the Spark context
+sc <- spark_connect(master = "yarn-client", version = "2.2.0",config=config) # using custom configs
+data_tbl = spark_read_parquet(sc,"data_tbl",datapath)
+data_tbl %>% group_by(g)
+         %>% summarize(coff = ml_logistic_regression(y~v1+v2+v3+v4+v5+v6+v7,fit_intercept =FALSE,family = "binomial")$coefficients)
 
 
 
 
-
-## end time
+#################################### end time ################################
 end_time <- Sys.time()
 t = as.numeric(end_time) - as.numeric(start_time)
 print(start_time)
