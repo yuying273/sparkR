3a9095381e63a3fc353cca7ae190da25d9110e87
Yuying Song <syysea@gmail.com>
2018-12-30 23:52:36 -0500

    Update readcsv.scala

--- readcsv.scala
+++ readcsv.scala
@@ -1,16 +1,19 @@
 
-## load a csv file
+// load a csv file
 val spark = org.apache.spark.sql.SparkSession.builder.
         master("yarn").
         appName("Spark CSV Reader").
         getOrCreate;
 
-//val df = spark.read.format("csv").option("header", "false").option("mode", "DROPMALFORMED").load("/wsc/song273/try2.csv")
 val df = spark.read.format("com.databricks.spark.csv").option("header", "false").option("inferSchema", "true").
                   load("/wsc/song273/try2.csv")        
 df.first()
+/////// layout of the data
 // number of rows
 df.count()
 // number of columns
 df.columns.size
-df2.printSchema()
+df.printSchema()
+df.show
+// groupby the conditional variable, the last column, and count the number of entries of column "_c21"
+df.groupBy("_c21").count().show
